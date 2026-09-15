fc2fc8cc615eed43af1d9db3f9e285a683b8e06a
Stephen McManus <steve.mcmanus@hotmail.co.uk>
2026-09-15 10:11:20 +0100

    Add Terraform management for Lambda IAM role

--- terraform/main.tf
+++ terraform/main.tf
@@ -34,3 +34,24 @@ resource "aws_lambda_function" "lex_orchestrator" {
     }
   }
 }
+
+resource "aws_iam_role" "lex_orchestrator" {
+  name = "lex_orchestrator-role-cad9eis1"
+  path = "/service-role/"
+
+  assume_role_policy = jsonencode({
+    Version = "2012-10-17"
+
+    Statement = [
+      {
+        Effect = "Allow"
+
+        Principal = {
+          Service = "lambda.amazonaws.com"
+        }
+
+        Action = "sts:AssumeRole"
+      }
+    ]
+  })
+}
